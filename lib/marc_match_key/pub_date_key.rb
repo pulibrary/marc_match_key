d3c826952678926d2dfb0f4196aee602c0c13db6
Mark Zelesky <mzelesky@princeton.edu>
2025-05-14 13:26:07 -0400

    title and pub date portions

---
+++ lib/marc_match_key/pub_date_key.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+module MarcMatchKey
+  ### Generates the publication date portion of a GoldRush key
+  class PubDateKey
+    attr_reader :record, :key
+
+    def initialize(record)
+      @record = record
+      @key ||= generate_key
+    end
+
+    private
+
+    ### follows GoldRush documentation, but the logic is not clear why
+    ###   Date2 is preferred over Date1; monographs only have a date in Date1
+    def generate_key
+      pub_date = pub_date_from_f008
+      pub_date ||= pub_date_from_pub_field(pub_date_26x_field)
+      pad_with_underscores(pub_date, 4)
+    end
+
+    def pub_date_from_f008
+      f008 = record['008']&.value
+      if f008 && f008[6] == 'r'
+        pub_date = f008[7..10]
+      elsif f008
+        pub_date = f008[11..14]
+      end
+      pub_date = nil if pub_date =~ /[^0-9]/
+      pub_date
+    end
+
+    ### GoldRush uses the first 264 field to appear in the record, which could
+    ###   end up using the copyright date or another type of date instead of
+    ###   publication date
+    def pub_date_26x_field
+      pub_date_from_f264 || record.fields('260').find { |f| f['c'] }
+    end
+
+    def pub_date_from_f264
+      preferred_order = %w[1 4 2 3 0]
+      f264 = record.fields('264').select { |f| f['c'] }
+
+      preferred_order.each do |indicator|
+        field = f264.find { |f| f.indicator2 == indicator }
+        return field if field
+      end
+      nil
+    end
+
+    def pub_date_from_pub_field(pub_field)
+      return '0000' if pub_field.nil?
+
+      subfc = pub_field['c'].dup
+      pub_date = subfc.gsub(/^.*([0-9]{4})[^0-9]*$/, '\1')
+      pub_date = '0000' if pub_date !~ /^[0-9]{4}$/
+      pub_date
+    end
+  end
+end
---
+++ spec/marc_match_key/functions_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+RSpec.describe 'MarcMatchFunctions' do
+  describe 'strip_punctuation' do
+    let(:replace_char) { '' }
+
+    context "when string starts with 'A'" do
+      let(:string) { 'A test' }
+
+      it 'removes the stop word' do
+        expect(strip_punctuation(string: string,
+                                 replace_char: replace_char)).to eq 'test'
+      end
+    end
+
+    context "when string starts with 'An'" do
+      let(:string) { 'An animal' }
+
+      it 'removes the stop word' do
+        expect(strip_punctuation(string: string,
+                                 replace_char: replace_char)).to eq 'animal'
+      end
+    end
+
+    context "when string starts with 'the'" do
+      let(:string) { 'the best' }
+
+      it 'removes the stop word' do
+        expect(strip_punctuation(string: string,
+                                 replace_char: replace_char)).to eq 'best'
+      end
+    end
+
+    context 'when string has all replaced characters' do
+      let(:string) { "Apples & p\u0022\u002aeaches %22%are g\u003bo\u005eo\u007cd in '{}p\u007ei\u00a9e" }
+
+      it 'replaces all characters correctly' do
+        expect(strip_punctuation(string: string,
+                                 replace_char: replace_char)).to eq 'Applesandpeachesaregoodinpie'
+      end
+    end
+  end
+
+  describe 'pad_with_underscores' do
+    let(:string) { 'spaces' }
+    let(:string_length) { 10 }
+
+    it 'adds underscores to make the string 10 characters long' do
+      expect(pad_with_underscores(string,
+                                  string_length)).to eq 'spaces____'
+    end
+  end
+
+  describe 'trim_max_field_length' do
+    let(:string) { 'a' * 32_001 }
+
+    it 'makes string length 32,000 characters' do
+      expect(trim_max_field_length(string).length).to eq 32_000
+    end
+  end
+
+  describe 'normalize_string_and_remove_accents' do
+    let(:string) { "piet\u00e1" }
+
+    it 'normalizes to :nfd and removes acute accent' do
+      expect(normalize_string_and_remove_accents(string)).to eq 'pieta'
+    end
+  end
+end
